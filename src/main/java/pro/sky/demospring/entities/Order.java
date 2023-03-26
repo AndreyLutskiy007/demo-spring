fc49d491abd4e300dd8883bb1733d459f1e84bf8
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-03-26 12:13:19 +0300

    HomeWork Spring

---
+++ src/main/java/pro/sky/demospring/entities/Order.java
@@ -0,0 +1,17 @@
+package pro.sky.demospring.entities;
+
+public class Order {
+    private Integer id;
+
+    public Order(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
